9e37255181f9522f69eba315cbac285ae9732a0c
Lyyynt <64420667+Lyyynt@users.noreply.github.com>
2020-11-14 00:06:53 +0700

    Update TagManager.java

--- ProjectOOP/src/Model/TagManager.java
+++ ProjectOOP/src/Model/TagManager.java
@@ -1,25 +1,46 @@
 package Model;
+
 import java.util.Set;
+import java.util.ArrayList;
 
 public class TagManager {
+    
     private Set<Tag> tagSet;
-    /*
-    Todo : Bổ sung các hàm thêm, xóa, sửa, tìm 1 tag vào tagset
-    */
-    public TagManager() {
-
-    }
-
-    public TagManager(Set<Tag> tagSet) {
-        this.tagSet = tagSet;
-    }
-
-    public Set<Tag> getTagSet() {
-        return tagSet;
-    }
-
-    public void setTagSet(Set<Tag> tagSet) {
-        this.tagSet = tagSet;
-    }
+	
+	ArrayList<Tag> tagList = new ArrayList<Tag>();
+	
+	public TagManager() {
+		
+	}
+	public TagManager(Set<Tag> tagSet) {
+		this.tagSet= tagSet;
+	}
+	
+	public Set<Tag> getTagSet(){
+		return tagSet;
+	}
+	
+	public void setTagSet(Set<Tag> tagSet) {
+		this.tagSet = tagSet;
+	}
+	
+//Thêm tag vào tagList 
+	public void addTag(Tag Tag) {
+	tagList.add((Tag) tagSet);
+	}
+	
+//Xoá tag khỏi tagList 
+	public void removeTag(Tag Tag) {
+	tagList.remove((Tag) tagSet);
+	}
+//Tìm Kiếm xem có tag trong tagList không, nếu có trả về true và hiện ra tag 
+	public Tag findTag(Tag String) {
+	tagList.contains((Tag) tagSet);
+	return (Tag) tagSet;
+	}
+//Ktra xem có tag đó trong tagList hay không 
+	public boolean isHaveThisTag(Tag String) {
+	return tagList.contains((Tag) tagSet);
+	}
 
 }
