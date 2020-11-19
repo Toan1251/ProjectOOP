0f1bd31c574ca8e1014af654f960836cc5caeec2
Toan1251 <45909891+Toan1251@users.noreply.github.com>
2020-11-19 21:59:55 +0700

    Update TagManager.java

--- ProjectOOP/src/Model/TagManager.java
+++ ProjectOOP/src/Model/TagManager.java
@@ -4,7 +4,7 @@ import java.util.Set;
 
 public class TagManager {
 
-    private Set<Tag> tagSet;
+    private Set<Tag> tagSet; // Danh sách hỗ trợ quản lý Tag(Đảm bảo không có tag nào trùng nhau trong Set)
 
 
     public TagManager() {
@@ -22,16 +22,17 @@ public class TagManager {
         this.tagSet = tagSet;
     }
 
-
+    // Thêm 1 Tag vào danh sách các Tag
     public void addTag(Tag tag) {
         tagSet.add(tag);
     }
 
-
+    // Xóa 1 Tag khỏi danh sách các Tag
     public void removeTag(Tag tag) {
         tagSet.remove(tag);
     }
-
+    
+    // Tìm kiếm Tag cần thiết trong danh sách Tag
     public Tag findTag(String tag ) {
         if(isHaveThisTag(tag)){
             for (Tag temp : tagSet) {
@@ -40,9 +41,11 @@ public class TagManager {
                 }
             }
         }
+        // Nếu không có Tag cần thiết trả về null
         return null;
     }
 
+    // Nếu Tag cần thiết có trong danh sách Tag trả về True
     public boolean isHaveThisTag(String tag) {
         Tag temp = new Tag(tag);
         return tagSet.contains(temp);
