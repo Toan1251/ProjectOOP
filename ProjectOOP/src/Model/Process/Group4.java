72485c47c1576dc3a8642a67871eddea717bcab9
Dxung <dung.lx183505@gmail.com>
2020-11-22 00:04:48 +0700

    upload

---
+++ ProjectOOP/src/Model/Process/Group4.java
@@ -0,0 +1,31 @@
+package Process;
+
+import Model.DataInput;
+
+import java.util.ArrayList;
+
+public class Group4 extends Group {
+    //override lại phương thức để lấy value ứng với key
+    @Override
+    public String replaceVariable(String key) {
+        return this.eachMap.get(key);
+    }
+
+    @Override
+    public void begin(ArrayList<DataInput> data) {
+        //thêm câu vào list sentences chung chờ xử lí
+        addGroupSentences("Được biết, trong tổng số <tong> mã thì dẫn đầu là là cổ phiếu <name> với mức tăng <percent>%, tương ứng tăng từ <begin> đồng lên <end> đồng. .");
+        addGroupSentences("Giữ vị trí quán quân về tăng giá tại HoSE là <name> với mức tăng <percent>%, tương ứng tăng từ <begin> lên <end> đồng.");
+        addGroupSentences("Đứng đầu danh sách tăng giá là mã <name>, tăng từ <begin> đồng lên <end> đồng, tương ứng với mức tăng <percent>%.");
+
+        // phương thức dùng để tạo 1 map chứa các giá trị cần thay và tên biến tương ứng.
+        this.eachMap = (new Rule2()).PushInMap(data);
+
+        //phương thức xử lí và đưa câu đã lựa chọn vào list chung để phục vụ việc tạo đoạn văn sau này
+        addSentences(getOutput());
+
+
+    }
+}
+
+//mức tăng cao nhất(về %)
---
+++ ProjectOOP/src/Model/Process/Rule7.java
@@ -0,0 +1,44 @@
+package Process;
+
+import Model.DataInput;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
+
+//nhóm top các cổ phiếu hàng không
+public class Rule7 extends Rules {
+
+
+    @Override
+    public Map<String, String> PushInMap(ArrayList<DataInput> data) {
+
+        ArrayList<DataInput> array = new ArrayList<>();
+        ArrayList<Rules> arr = new ArrayList<>();
+
+        //tìm các đối tượng có tên như dưới và đưa tất cả vào list arr;
+        array.add(filterByName(data,"HVN"));
+        array.add(filterByName(data,"VJC"));
+
+        for (int i=0;i<array.size();i++){
+            double tmp= array.get(i).getClose()-array.get(i).getOpen();
+            arr.add(new Rules(i,tmp,0));
+        }
+
+        Collections.sort(arr,new SortByChange());
+        for(int i=0;i<arr.size();i++) {
+            map.put("nameA" +(i+1), array.get(arr.get(i).getIndex()).getName());
+            map.put("numA"+(i+1),Double.toString(Math.abs(arr.get(i).getValueChange())*1000));
+            if(arr.get(i).getValueChange()>=0){map.put("Alink"+(i+1),"tăng");}
+            if(arr.get(i).getValueChange()<0){map.put("Alink"+(i+1),"giảm");}
+            map.put("level"+(i+1),level(arr.get(i).getValueChange()));
+        }
+
+        Collections.sort(array,new DataSortByVolumeValue());
+        for(int i=0;i<array.size();i++) {
+            map.put("AVname" + (i + 1),array.get(i).getName());
+            map.put("AVnum"+(i+1),Integer.toString(array.get(i).getVolume()));
+        }
+
+        return map;
+    }
+}
