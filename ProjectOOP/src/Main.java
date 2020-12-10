56dcc99210e1523051508c40da0583d28f35176c
Toan1251 <45909891+Toan1251@users.noreply.github.com>
2020-12-11 00:07:07 +0700

    Add crawler Data from CafeF

--- ProjectOOP/src/Main.java
+++ ProjectOOP/src/Main.java
@@ -1,8 +1,27 @@
 package ProjectOOP.src;
 
+import ProjectOOP.src.crawler.ExtractData;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
 public class Main {
 
     public static void main(String[] args) {
+        ExtractData now;
+        try{
+            now = new ExtractData();
+        }catch (UnknownHostException e){
+            e.printStackTrace();
+            System.out.println("No internet Connection");
+            //todo: View cần hiển thị 1 thông báo không có kết nối Internet
+        }catch (IOException e){
+            e.printStackTrace();
+            System.out.println("No file in the Directory");
+            //todo: View hiển thị missing file cho người dùng
+        }
 
+
+        //todo: Xây dựng luồng hđ cho dữ liệu
     }
 }
