653e1c34381d4e5e5574cbed57f19391603d95ff
TaHung1 <68838758+TaHung1@users.noreply.github.com>
2020-12-15 08:17:54 +0700

    Update Controller

--- Controller.java
+++ Controller.java
@@ -1,4 +1,5 @@
 package ProjectOOP.src.Controller;
+
 import Model.DataFileOutput;
 import View.*;
 
@@ -11,31 +12,28 @@ public class Controller {
 	}
 
 	// Gửi Request về DataFileOutPut
-	// request là String mà người dùng nhập ở Search
-	public String sentRequest(DataFileOutput dfo) {
-		String request = ViewBase.userInputString;
-	
-		//Còn trường hợp nhấn vào các Button Tag, quy ra thanh String
-		
+	// request là String mà người dùng nhập ở Search, hoặc String ở TagView
+	public String sentRequest(DataFileOutput dfo, int id) {
+		String request;
+		if (id == 1) {
+			request = ViewBase.userInputString;
+		} else if (id == 2) {
+			request = TagView.tagString;
 		}
-	
-
-		
+		return request;
+	}
 
 	// Gửi trả dữ liệu về View
 	public String sentData(ViewBase viewbase, DataFileOutput dfo) {
 		String outputData = dfo.respond(request);
 		return outputData;
 	}
+
 	// Hiện kết quả lên Label Result
 	public void show(ViewBase viewbase) {
 		DataFileOutput dfo = new DataFileOutput();
-		viewbase.showData(sentData(viewbase,dfo));
-		
-		
+		viewbase.showData(sentData(viewbase, dfo));
+
 	}
-
-		
-
 
 }
