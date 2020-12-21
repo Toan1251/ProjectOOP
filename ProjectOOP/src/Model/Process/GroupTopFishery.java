3f6333f5d2bbabb47aaa66da1d63ed0aed64a9e2
Dxung <dung.lx183505@gmail.com>
2020-12-21 23:52:41 +0700

    fix fishery

--- ProjectOOP/src/Model/Process/GroupTopFishery.java
+++ ProjectOOP/src/Model/Process/GroupTopFishery.java
@@ -15,7 +15,7 @@ public class GroupTopFishery extends Group {
     public String begin(List<DataOutput> data) {
         //thêm câu vào list sentences chung chờ xử lí
         addGroupSentences("Riêng nhóm thủy sản, <nameF1> đang dẫn đầu khi <Flink1> <level1> <numF1> đồng, ngay sau đó là <nameF2> <Flink2> <level2> <numF2> đồng.");
-        addGroupSentences(" Bên cạnh đó, với việc tham gia các hiệp định thương mại thì nhóm ngành xuất khẩu như thủy sản cũng được đánh giá cao, nổi bật trong đó là <nameF1> đang dẫn đầu khi <Flink1> <level1> <numF1> đồng, ngay sau đó là <nameF2> <Flink2> <level2> <numBank2> đồng.");
+        addGroupSentences(" Bên cạnh đó, với việc tham gia các hiệp định thương mại thì nhóm ngành xuất khẩu như thủy sản cũng được đánh giá cao, nổi bật trong đó là <nameF1> đang dẫn đầu khi <Flink1> <level1> <numF1> đồng, ngay sau đó là <nameF2> <Flink2> <level2> <numF2> đồng.");
 
         // phương thức dùng để tạo 1 map chứa các giá trị cần thay và tên biến tương ứng.
         this.eachMap = (new RuleFischery()).PushInMap(data);
