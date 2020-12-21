c9416c172cd38ec93197bf1fb6eb6acec73d6ac4
Dxung <dung.lx183505@gmail.com>
2020-12-21 23:28:28 +0700

    fix

--- ProjectOOP/src/Model/Process/GroupTopVolume.java
+++ ProjectOOP/src/Model/Process/GroupTopVolume.java
@@ -14,9 +14,9 @@ public class GroupTopVolume extends Group {
     @Override
     public String begin(List<DataOutput> data) {
         //thêm câu vào list sentences chung chờ xử lí
-        addGroupSentences("Về khối lượng khớp lệnh của từng mã trong phiên này, <vol1> vươn lên dẫn đầu với hơn <v1> cổ phiếu, <vol2> tiếp tục giao dịch sôi động với gần <v2> cổ phiếu được khớp lệnh và không còn dư bán, <vol3> cũng có giao dịch cao là hơn <v3> cổ phiếu.");
-        addGroupSentences("Tính cho đến thời điểm này, <vol1> vẫn là cổ phiếu thanh khoản nhất thị trường với khối lượng giao dịch lớn và diễn biến rất sôi động, cụ thể là <v1> cổ phiếu được khớp lệnh, tiếp đến là <vol2> với <v2> đơn vị/mã và ngay sau đó là <vol3> với <v3> cổ phiếu được khớp.");
-        addGroupSentences("Phiên này, <vol1>, <vol2> và <vol3> dẫn đầu thị trường về lượng khớp lệnh, trong đó phải kể đến <vol1> với <v1> đơn vị được chuyển nhượng. ");
+        addGroupSentences("Về khối lượng khớp lệnh của từng mã trong phiên này, <vol1> vươn lên dẫn đầu với hơn <v1> cổ phiếu, <vol2> tiếp tục giao dịch sôi động với gần <v2> cổ phiếu được khớp lệnh và không còn dư bán, <vol3> cũng có giao dịch là hơn <v3> cổ phiếu. Hai mã cổ phiếu <vol4> và <vol5> theo sau với lần lượt là <v4> và <v5> cổ phiếu được khớp. ");
+        addGroupSentences("Tính cho đến thời điểm này, <vol1> vẫn là cổ phiếu thanh khoản nhất thị trường với khối lượng giao dịch lớn và diễn biến rất sôi động, cụ thể là <v1> cổ phiếu được khớp lệnh, tiếp đến là <vol2> với <v2> đơn vị/mã, <vol3> với <v3> đơn vị/mã. Ngay sau đó là <vol4> và <vol5> với lần lượt <v4> và <v5> cổ phiếu được khớp. ");
+        addGroupSentences("Phiên này, <vol1>, <vol2> và <vol3> dẫn đầu thị trường về lượng khớp lệnh, trong đó phải kể đến <vol1> với <v1> đơn vị được chuyển nhượng, kế đến là <vol2> với <v2> đơn vị và <vol3> với <v3> đơn vị. Tuy không bằng các mã trên nhưng <vol4> với <v4> đơn vị/mã và <vol5> với <v5> đơn vị/ mã cũng đáng được nói đến. ");
 
         // phương thức dùng để tạo 1 map chứa các giá trị cần thay và tên biến tương ứng.
         this.eachMap = (new RuleTopVol()).PushInMap(data);
